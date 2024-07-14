00be7b657b8876749b7d2da7a8ad5c2fb314b73e
David George Williams <david@cynthiasystems.com>
2024-07-14 10:13:21 +0100

    Add container-crane class to DOTA-YOLO conversion mapping

--- src/01_convert_dota_to_yolo.py
+++ src/01_convert_dota_to_yolo.py
@@ -40,18 +40,19 @@ def convert_dota_to_yolo(dota_label_path, image_path, output_path):
         "baseball-diamond": 0,
         "basketball-court": 1,
         "bridge": 2,
-        "ground-track-field": 3,
-        "harbor": 4,
-        "helicopter": 5,
-        "large-vehicle": 6,
-        "plane": 7,
-        "roundabout": 8,
-        "ship": 9,
-        "small-vehicle": 10,
-        "soccer-ball-field": 11,
-        "storage-tank": 12,
-        "swimming-pool": 13,
-        "tennis-court": 14
+        "container-crane": 3,
+        "ground-track-field": 4,
+        "harbor": 5,
+        "helicopter": 6,
+        "large-vehicle": 7,
+        "plane": 8,
+        "roundabout": 9,
+        "ship": 10,
+        "small-vehicle": 11,
+        "soccer-ball-field": 12,
+        "storage-tank": 13,
+        "swimming-pool": 14,
+        "tennis-court": 15
     }
 
     yolo_lines = []
